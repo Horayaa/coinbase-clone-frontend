192b9599a3fe4ff62fe216acca8ccc7aa322c862
Malcolmxx3 <adamr7177@gmail.com>
2026-05-02 22:26:53 +0000

    Api request fixes

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,11 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 import { AuthProvider } from "./context/AuthProvider";
+import axios from "axios";
 import App from "./App";
 import "./index.css";
+
+axios.defaults.baseURL = import.meta.env.VITE_API_URL;
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
--- vite.config.ts
+++ vite.config.ts
@@ -5,15 +5,15 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://coinbase-clone-api-bevo.onrender.com',
-        changeOrigin: true,
-        secure: false,
-        // Optionally rewrite the path if your backend doesn't use /api
-        // rewrite: (path) => path.replace(/^\/api/, ''),
-      },
-    },
-  },
+  // server: {
+  //   proxy: {
+  //     '/api': {
+  //       target: 'https://coinbase-clone-api-bevo.onrender.com',
+  //       changeOrigin: true,
+  //       secure: false,
+  //       // Optionally rewrite the path if your backend doesn't use /api
+  //       // rewrite: (path) => path.replace(/^\/api/, ''),
+  //     },
+  //   },
+  // },
 })
